f0673199a285d08d68965f68acf0573f9db3c3da
Ubuntu <ubuntu@ip-172-31-46-83.ec2.internal>
2020-04-02 00:39:07 +0000

    commit message

---
+++ ICS3UUnit2O1.java
@@ -0,0 +1,20 @@
+// Copyright (c) Ac industries All rights reserved  
+//
+// Created by: Andrew Coxall
+// Created on: April 1, 2020
+// Area and perimeter using pie
+
+import java.lang.Math;
+
+
+public class ICS3UUnit2O1 {
+	public static void main(String[] args) {
+
+    	double area1 = Math.PI*15.0;
+    	double area2 = Math.pow(area1,2);
+    	double perimeter = 2*Math.PI*15.0;
+    	
+    	System.out.println("Area is " + area2);
+    	System.out.println("Perimeter is " + perimeter);
+	}
+}
